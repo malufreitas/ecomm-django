42b3309701e05b125c85fb5dfe56be34d66cd039
malufreitas <malufrt@Hotmail.com>
2019-09-26 10:10:09 -0300

    Criando view simples com jinja
    Jinja - Linguagem de template do django

---
+++ products/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from products.models import Product
+
+# Create your views here.
+
+# View para listas os produtos
+# Toda view recebe um request como argumento e precisa retornar uma resposta
+def list_products(request):
+    # Lendo todos os produtos
+    products = Product.objects.all()
+
+    # O contexto é sempre um dicionario
+    context = {
+        'products': products
+    }
+
+    # Retorna um render(request, nomeDoTemplate, contexto)
+    # Criar a pasta do template na mão, na pasta products: uma pasta templates -> products -> list.html
+    return render(request, 'products/list.html', context=context)
